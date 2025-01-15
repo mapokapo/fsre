54d6b447bcec5e61ad67bef53e5d3f36ebf59720
mapokapo <leopetrovic11@gmail.com>
2025-01-15 20:35:57 +0100

    UUI - lab1py: feat: parse search space

--- uui/homework/lab1py/solution.py
+++ uui/homework/lab1py/solution.py
@@ -1,9 +1,16 @@
 from cli import get_args
+from parsing import parse_search_space
 
 args = get_args()
 
+search_space = parse_search_space(args.ss.readlines())
+
 def main():
-  print(args)
+  print(f'Start: {search_space[0][0]}')
+  print(f'End: {search_space[0][1]}')
+  print("Search space:")
+  for key, values in search_space[1].items():
+    print(f'\t{key}: {values}')
 
 if __name__ == "__main__":
   main()
